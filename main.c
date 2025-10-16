7bb93b6f020d518fc30aa86c873a2c50804ecde4
doudoumai007 <lipeiyao_@outlook.com>
2025-10-16 15:46:49 +0200

    build the initial main structure

--- main.c
+++ main.c
@@ -1,4 +1,21 @@
-int main(int ac, char *av[])
+#include "minishell.h"
+
+int main(int ac, char *av[], char **env)
 {
-    
+	char	*line;
+	// t_data	*data;
+
+	(void)ac;
+	(void)av;
+	(void)env;
+	while (1)
+	{
+		line = readline("minishell> ");
+		if (*line)
+			add_history(line);
+		printf("You typed: %s\n", line);
+		// parseline(line);
+		// exec(&data);
+		free(line);
+	}
 }
