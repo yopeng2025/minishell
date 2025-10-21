c30e7d02caad94f23ad93654c9ca9a79bf1f9083
Yongjia Peng <yopeng@student.42.fr>
2025-10-21 18:42:33 +0200

    WIP: work in progress before rebase

--- minishell.h
+++ minishell.h
@@ -55,12 +55,11 @@ void	quoting_choice(bool *dq, bool *sq, int *index, char c);
 int		open_quote(t_data *data, char *line);
 bool	create_list_token(t_token **head_token, char *line);
 int		add_dollar(char *line, int *index, char **str, t_data *data);
-static int	in_env(t_data *data, char *line, int size, char **str);
 int		replace_dollar(char **line, t_data *data);
-static int	end_word(char *str, char *env);
 int		exist_in_env(char *line, int *i, t_data *data);
 char	*get_dollar_word(char *line, int size);
 char	*get_elem_env(t_list *env, char *key);
+
 //token
 void	print_token_list(t_token *head);
 void	free_token_list(t_token **head_token);
