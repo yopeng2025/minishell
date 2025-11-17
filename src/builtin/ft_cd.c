f4c8ee56cd3b750f5015a9e574f95ea1d44c8bc7
doudoumai007 <lipeiyao_@outlook.com>
2025-11-17 19:20:25 +0100

    update the error nofit when cd with a wrong path

--- src/builtin/ft_cd.c
+++ src/builtin/ft_cd.c
@@ -1,38 +1,35 @@
 #include "minishell.h"
 
-// void	update_oldpwd(t_data *data)
-// {
-// 	t_list	*tmp_env;
-// 	char	*old_pwd;
+static void	update_oldpwd(t_data *data)
+{
+	t_list	*tmp_env;
+	char	*old_pwd;
 
-// 	tmp_env = data->env;
-// 	old_pwd = NULL;
-// 	if (!ft_strncmp(tmp_env->str, "PWD=", 4))
-// 		old_pwd = tmp_env->str;
-// 	tmp_env = tmp_env->next;
-// 	while (tmp_env != data->env)
-// 	{
-// 		if (!ft_strncmp(tmp_env->str, "PWD=", 4))
-// 		{
-// 			old_pwd = tmp_env->str;
-// 			break ;
-// 		}
-// 		tmp_env = tmp_env->next;
-// 	}
-// 	if (!old_pwd)
-// 		export("OLDPWD=", &data->env);
-// 	if (old_pwd)
-// 	{
-// 		old_pwd = ft_strjoin("OLD", old_pwd);
-// 		if (!old_pwd)
-// 		{
-// 			//打印错误信息
-// 			return ;
-// 		}
-// 		export(old_pwd, &data->env);
-// 	}
-// 	free(old_pwd);
-// }
+	tmp_env = data->env;
+	old_pwd = NULL;
+	if (!ft_strncmp(tmp_env->str, "PWD=", 4))
+		old_pwd = tmp_env->str;
+	tmp_env = tmp_env->next;
+	while (tmp_env != data->env)
+	{
+		if (!ft_strncmp(tmp_env->str, "PWD=", 4))
+		{
+			old_pwd = tmp_env->str;
+			break ;
+		}
+		tmp_env = tmp_env->next;
+	}
+	if (!old_pwd)
+		export("OLDPWD=", &data->env);
+	if (old_pwd)
+	{
+		old_pwd = ft_strjoin("OLD", old_pwd);
+		if (!old_pwd)
+			free_all(data, ERR_MALLOC, EXT_MALLOC);
+		export(old_pwd, &data->env);
+	}
+	free(old_pwd);
+}
 
 static void	update_cwd(t_data *data, char *path)
 {
@@ -40,7 +37,7 @@ static void	update_cwd(t_data *data, char *path)
 	char	*pwd;
 
 	(void)path;
-	// update_oldpwd(data); 这个函数可以去掉不写 因为cd -不是相对或绝对路径
+	update_oldpwd(data); //这个函数可以去掉不写 因为cd -不是相对或绝对路径
 	if (!getcwd(cwd, PATH_MAX))
 	{
 		print_error(path);
@@ -70,7 +67,11 @@ int	ft_cd(t_data *data, char **cmd_param)
 	}
 	if (chdir(cmd_param[1]) == -1)
 	{
-		print_error(cmd_param[1]);
+		write(2, "minishell: ", 12);
+		write(2, cmd_param[0], ft_strlen(cmd_param[0]));
+		write(2, ": ", 3);
+		write(2, cmd_param[1], ft_strlen(cmd_param[1]));
+		write(2, ": No such file or directory\n", 29);
 		return (1);
 	}
 	update_cwd(data, cmd_param[1]);
